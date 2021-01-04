816bb3fb80a4b260c564930c7bc9a9aa54d73194
sydney miranda <sydmiranda@icloud.com>
2021-01-04 20:52:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].is a 
+      if src[row_index][element_index].is_a? String 
             string_array << src[row_index][element_index]
                 element_index += 1
     end
