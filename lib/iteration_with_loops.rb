05191309ade16c5f9719d5e8fc3acdadce17c5a7
sydney miranda <sydmiranda@icloud.com>
2021-01-04 20:40:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,8 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index] === ''
+            new_string.concat.join(src[row_index][element_index])
     end
-    new_string.concat(src[row_index][element_index])
     element_index +=1
   end
   row_index +=1
